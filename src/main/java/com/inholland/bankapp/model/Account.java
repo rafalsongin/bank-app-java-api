34427e83bd809944ac2e96fc61523a695652a9af
Mariia <705283@student.inholland.nl>
2024-05-01 18:21:12 +0200

    Update Account.java

--- src/main/java/com/inholland/bankapp/model/Account.java
+++ src/main/java/com/inholland/bankapp/model/Account.java
@@ -1,12 +1,22 @@
 package com.inholland.bankapp.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Setter
 @Getter
+@NoArgsConstructor
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
     private int account_id;
     private int customer_id;
     private String IBAN;
