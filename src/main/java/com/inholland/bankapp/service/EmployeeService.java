0c1e0592efcc56911f12a6bddbb1318530e2aee6
Mariia <705283@student.inholland.nl>
2024-05-17 19:44:25 +0200

    account type

--- src/main/java/com/inholland/bankapp/service/EmployeeService.java
+++ src/main/java/com/inholland/bankapp/service/EmployeeService.java
@@ -20,28 +20,23 @@ public class EmployeeService {
     @Autowired
     private final EmployeeRepository employeeRepository;
 
-    // Constructor injection is recommended
     @Autowired
     public EmployeeService(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
 
-    // A method to find all employees
     public List<Employee> findAllEmployees() {
         return employeeRepository.findAll();
     }
 
-    // A method to find an employee by ID
     public Optional<Employee> findEmployeeById(int id) {
         return employeeRepository.findById(id);
     }
 
-    // A method to save or update an employee
-    public Employee saveOrUpdateEmployee(Employee employee) {
+    public Employee saveEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
 
-    // A method to delete an employee by ID
     public void deleteEmployee(int id) {
         employeeRepository.deleteById(id);
     }
