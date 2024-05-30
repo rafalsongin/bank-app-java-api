5e6f614e35ac33091f5168640fc5662b8ade55f4
Rafal Songin <rafal.songin@gmail.com>
2024-05-30 03:58:04 +0200

    Testing Github Actions 4

--- src/main/java/com/inholland/bankapp/security/SecurityConfig.java
+++ src/main/java/com/inholland/bankapp/security/SecurityConfig.java
@@ -20,7 +20,7 @@ import java.util.Arrays;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    // Security configuration (TEST 3)
+    // Security configuration (TEST 4)
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
