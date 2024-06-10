c9d5759e7f88d42df44b7af088609b8b87a36cb1
Rafal Songin <rafal.songin@gmail.com>
2024-06-10 22:09:29 +0200

    Development fix

--- src/test/java/com/inholland/bankapp/cucumber_testing/BaseStepDefinitions.java
+++ src/test/java/com/inholland/bankapp/cucumber_testing/BaseStepDefinitions.java
@@ -1,11 +1,29 @@
 package com.inholland.bankapp.cucumber_testing;
 
 import io.cucumber.spring.CucumberContextConfiguration;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class BaseStepDefinitions {
+    @LocalServerPort
+    protected int port;
+
+    @Getter
+    @Setter
+    protected ResponseEntity<String> response;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Running tests on port: " + port);
+    }
 
 }
-
